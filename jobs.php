ae7930a23884da496355faea7a51760e4748a3bd
Manuel Elizondo <elizondo.manuel.101c@gmail.com>
2019-05-23 23:18:36 -0700

    Segundo commit clase 16

---
+++ jobs.php
@@ -0,0 +1,34 @@
+<?php
+$jobs = [
+    [
+        'title' => 'php Developer',
+        'descripcion' => 'meh!!',
+        'visible' => true,
+        'months' => 12
+    ],
+    [
+        'title' => 'python Developer',
+        'descripcion' => 'meh!!',
+        'visible' => true,
+        'months' => 16
+    ],
+    [
+        'title' => 'Devops',
+        'descripcion' => 'meh!!',
+        'visible' => false,
+        'months' => 5
+    ],
+    [
+        'title' => 'Node Dev',
+        'descripcion' => 'meh!!',
+        'visible' => true,
+        'months' => 24
+    ],
+    [
+        'title' => 'Frontend Dev',
+        'descripcion' => 'meh!!', 
+        'visible' => true,
+        'months' => 19
+    ]];
+
+?>
